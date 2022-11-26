c456d31216211b3daaea7e84c9e9c6d604bdb533
weathon <wguo6358@gmail.com>
2022-11-26 06:36:56 +0000

    Text extracting done

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI, File, UploadFile
+from PyPDF2 import PdfFileReader
+import io
 
 app = FastAPI()
 
@@ -8,6 +10,22 @@ async def root():
     return {"message": "I am glad you are here, we have some cookies, come here!"}
 
 
-@app.get("/contractKeywordChecking")
+@app.post("/contractKeywordChecking")
 async def contract(file: UploadFile):
-    return {"message": "I am glad you are here, we have some cookies, come here!"}
+    parts=[]
+    def visitor_body(text, cm, tm, fontDict, fontSize):
+        parts.append(text)
+
+    request_object_content = await file.read()
+    pdf = PdfFileReader(io.BytesIO(request_object_content))
+    pages = pdf.pages
+
+    for i in pages:
+        i.extract_text(visitor_text=visitor_body)
+    words = []
+    for i in parts:
+        words.extend(i.split(" "))
+    print(words[:100])
+    return {
+        "Number of Pages":len(pages)
+    }
